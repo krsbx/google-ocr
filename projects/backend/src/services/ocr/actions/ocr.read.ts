d63049e45e27039f821eb13a026844f1b1730f13
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2024-05-16 16:18:32 +0700

    fix(be): validations error

--- projects/backend/src/services/ocr/actions/ocr.read.ts
+++ projects/backend/src/services/ocr/actions/ocr.read.ts
@@ -1,26 +1,38 @@
 import { BlazeCreator, z } from '@busy-hour/blaze';
 import OCR from '../utils/google';
 
-const bodySchema = z.object({
-  file: z
-    .custom<File>((data) => data)
-    .openapi({
-      type: 'file',
-      example: {
-        size: 1729,
-        type: 'video/mp2t',
-        name: 'users.create.ts',
-        lastModified: 1715645170595,
-      } as File,
-    }),
+const readValidator = BlazeCreator.action.validator({
+  body: z.object({
+    file: z
+      .custom<File>()
+      .transform((data, ctx) => {
+        if (!data.type.includes('pdf')) {
+          ctx.addIssue({
+            code: 'custom',
+            message: 'Only PDF files are allowed',
+          });
+        }
+
+        return data;
+      })
+      .openapi({
+        example: {
+          size: 1729,
+          type: 'application/pdf',
+          name: 'file.pdf',
+          lastModified: 1715645170595,
+        } as File,
+      }),
+  }),
 });
 
 const readOcr = BlazeCreator.action({
   rest: 'POST /',
-  // validator: readValidator,
-  throwOnValidationError: false,
+  validator: readValidator,
+  throwOnValidationError: true,
   async handler(ctx) {
-    const { file } = (await ctx.request.body()) as z.infer<typeof bodySchema>;
+    const { file } = await ctx.request.body();
+
     const buffer = await file.arrayBuffer();
 
     const results = await OCR.readFile(Buffer.from(buffer));
